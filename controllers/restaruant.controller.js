85cfd4534ccf2782123550cbb1eeb0b7f8fe90c4
Achinta Sharma <23achinta@gmail.com>
2022-10-08 20:43:52 +0530

    routes for adding , fetching restaurants added.

--- controllers/restaruant.controller.js
+++ controllers/restaruant.controller.js
@@ -1,12 +1,15 @@
+const dbConfig = require('../configs/db.config');
 const Restaurant = require('../models/restaruant.model');
 
 exports.addRestaurant = async (req, res) => {
     console.log(req.body)
-    if (!req.body) {
+    if (Object.keys(req.body).length === 0) {
+        console.log("trapped");
         return res.status(400).send({
             message: "Content cannot be empty"
         })
-    } //Check
+    };
+
     const restaurantObj = {
         name: req.body.name,
         description: req.body.description,
@@ -18,13 +21,32 @@ exports.addRestaurant = async (req, res) => {
     };
 
     try {
-        const restaurant = Restaurant.create(restaurantObj);
+        const restaurant = await Restaurant.create(restaurantObj);
 
         res.status(200).send(restaurant);
     } catch (err) {
         console.log("Error while creating restaurant: ", err.message);
         return res.status(500).send({
             message: "Some error occured while creating the Restaurant"
+        })
+    }
+}
+
+exports.getAllRestaruants = async (req, res) => {
+    try {
+        const restaurants = await Restaurant.find();
+
+
+        return res.status(400).send({
+            restaurants: restaurants,
+            message: "Restaurants fetched successfully"
+        })
+
+
+    } catch (err) {
+        console.log("Error while fetching restaurant: ", err.message);
+        return res.status(500).send({
+            message: "Some error occured while fetching the Restaurants"
         })
     }
 }
--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -2,4 +2,6 @@ const restaruantController = require('../controllers/restaruant.controller');
 
 module.exports = (app) => {
     app.post("/findrestaurant/api/v1/restaurant/add/", restaruantController.addRestaurant);
+
+    app.get("/findrestaurant/api/v1/restaurant/", restaruantController.getAllRestaruants);
 }
