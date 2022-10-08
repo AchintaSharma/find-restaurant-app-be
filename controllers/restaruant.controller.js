dd632d81a5f61e28219d20a6d809cfa84a918f99
Achinta Sharma <23achinta@gmail.com>
2022-10-08 22:33:52 +0530

    apis for restaurants search by id, rating, category added.

--- controllers/restaruant.controller.js
+++ controllers/restaruant.controller.js
@@ -2,7 +2,7 @@ const dbConfig = require('../configs/db.config');
 const Restaurant = require('../models/restaruant.model');
 
 exports.addRestaurant = async (req, res) => {
-    console.log(req.body)
+
     if (Object.keys(req.body).length === 0) {
         console.log("trapped");
         return res.status(400).send({
@@ -32,21 +32,82 @@ exports.addRestaurant = async (req, res) => {
     }
 }
 
-exports.getAllRestaruants = async (req, res) => {
+exports.findAllRestaurants = async (req, res) => {
     try {
         const restaurants = await Restaurant.find();
 
-
-        return res.status(400).send({
+        return res.status(200).send({
             restaurants: restaurants,
             message: "Restaurants fetched successfully"
         })
-
 
     } catch (err) {
         console.log("Error while fetching restaurant: ", err.message);
         return res.status(500).send({
             message: "Some error occured while fetching the Restaurants"
+        })
+    }
+}
+
+exports.findRestaurantCategories = async (req, res) => {
+    try {
+        const restaruantCategories = await Restaurant.find().distinct('category');
+
+        return res.status(200).send(restaruantCategories);
+
+    } catch (err) {
+        console.log("Error while fetching restaurant categories: ", err.message);
+        return res.status(500).send({
+            message: "Some internal server error occured while fetching categories"
+        })
+    }
+}
+
+exports.findRestaurantByCategory = async (req, res) => {
+    try {
+        const restaruants = await Restaurant.find({ category: req.params.categoryName});
+
+        return res.status(200).send(restaruants);
+        
+
+    } catch (err) {
+        console.log("Error while fetching restaurant categories: ", err.message);
+        return res.status(500).send({
+            message: "Some error occured while fetching the Restaurant"
+        })
+    }
+}
+
+exports.findRestaurantById = async (req, res) => {
+    try {
+        const restaurant = await Restaurant.find({ _id: req.params.id});
+
+        if(!restaurant) {
+            return res.status(404).send({
+                message : "No Restaurant found with the given ID."
+            })
+        }
+        return res.status(200).send(restaurant);
+        
+    } catch (err) {
+        console.log("Error while fetching restaurant : ", err.message);
+        return res.status(500).send({
+            message: "Some error occured while fetching the Restaurant"
+        })
+    }
+}
+
+
+exports.findRestaurantByRating = async (req, res) => {
+    try {
+        const restaurant = await Restaurant.find({ rating : {$gte : req.params.ratingValue}});
+
+        return res.status(200).send(restaurant);
+        
+    } catch (err) {
+        console.log("Error while fetching restaurant : ", err.message);
+        return res.status(500).send({
+            message: "Some error occured while fetching the Restaurant"
         })
     }
 }
--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -1,7 +1,15 @@
-const restaruantController = require('../controllers/restaruant.controller');
+const restaurantController = require('../controllers/restaruant.controller');
 
 module.exports = (app) => {
-    app.post("/findrestaurant/api/v1/restaurant/add/", restaruantController.addRestaurant);
+    app.post("/findrestaurant/api/v1/restaurant/add/", restaurantController.addRestaurant);
 
-    app.get("/findrestaurant/api/v1/restaurant/", restaruantController.getAllRestaruants);
+    app.get("/findrestaurant/api/v1/restaurant/", restaurantController.findAllRestaurants);
+
+    app.get("/findrestaurant/api/v1/restaurant/categories", restaurantController.findRestaurantCategories);
+
+    app.get("/findrestaurant/api/v1/restaurant/categories/:categoryName", restaurantController.findRestaurantByCategory);
+
+    app.get("/findrestaurant/api/v1/restaurant/:id", restaurantController.findRestaurantById);
+
+    app.get("/findrestaurant/api/v1/restaurant/rating/:ratingValue", restaurantController.findRestaurantByRating);
 }
